76fb2da2d66c3a8c7a148fb89f857f7902147fb3
MustafaTheEngineer <mustafaesat01@gmail.com>
2022-06-29 10:48:25 +0300

    1-Beginning

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'movies',
+    templateUrl: './movies.component.html'
+})
+
+export class MoviesComponent {
+    num:number=5;
+    constructor() {
+        
+    }
+}
