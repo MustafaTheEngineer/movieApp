bbe523ff9d5dccd9f76d1d540e48fdeab93b0dd2
MustafaTheEngineer <mustafaesat01@gmail.com>
2022-06-30 01:06:31 +0300

    5- inputs

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -5,23 +5,27 @@ import { Movies } from "../movie.datasource";
 @Component({
     selector: 'movies',
     templateUrl: './movies.component.html',
-    styles: [`
-        h2 {
-            color: blue;
-        }
-    `]
+    styleUrls: ['./movies.component.css']
 })
 
 export class MoviesComponent {
     private title:string = "Naber";
 
     movies:Movie[] = Movies;
+    selectedMovie:Movie;
 
     constructor() {
-        
+        this.selectedMovie = {
+            id:0,
+            name: ""
+        }
     }
 
     get titleName(){
         return this.title; 
+    }
+
+    onSelect(movie:Movie):void{
+        this.selectedMovie = movie;
     }
 }
